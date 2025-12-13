19bfefa1cbeec9bb6c189c585d22a757aaec1fa0
Hashish03 <Khoasehashatsi40@gmail.com>
2025-12-13 05:51:29 +0200

    fixed the service

--- src/components/pages/ServicePage.jsx
+++ src/components/pages/ServicePage.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 import { Check, Code, Smartphone, ShoppingCart, Palette, Wrench, Server, Workflow, Coins, Database, ShieldCheck } from 'lucide-react';
 
 
